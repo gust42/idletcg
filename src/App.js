93f7e1db5e8269ff1da8e1d94bc8c9e32ad2b7d8
Gusten Andersson <gust42@gmail.com>
2019-07-04 22:13:01 +0200

    Tabs are now clickable!

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import React from 'react';
-import MessageHandler from './logic/messagehandler';
-
-import Button from './components/button';
+import React, { useState } from 'react';
 import GameLoop from './logic/gameloop';
 import './App.css';
 import MessageBox from './components/messagebox';
 import ResourceView from './components/resourceview';
-import useGameRule from './hooks/usegamerule';
 import useGameState from './hooks/usegamestate';
 import Tab from './components/tab';
+import PacksTab from './components/packstab';
+import TradebinderTab from './components/tradebindertab';
 
 const gameLoop = GameLoop.getInstance();
 gameLoop.start();
 function App() {
+  const [currentTab, setCurrentTab] = useState(<PacksTab />);
   const gameState = useGameState();
-  const packCostRule = useGameRule('PackCost');
-  const goodSellValueRule = useGameRule('GoodCardSellValue');
-  const badSellValueRule = useGameRule('BadCardSellValue');
-  const metaSellValueRule = useGameRule('MetaCardSellValue');
 
-  function openPack(amount) {
-    MessageHandler.recieveMessage('openpack', amount ? amount : 1);
-  }
-
-  function sellBadCards(amount) {
-    MessageHandler.recieveMessage('sellbadcards', amount ? amount : 1);
-  }
-
-  function sellGoodCards(amount) {
-    MessageHandler.recieveMessage('sellgoodcards', amount ? amount : 1);
-  }
-
-  function sellMetaCards(amount) {
-    MessageHandler.recieveMessage('sellmetacards', amount ? amount : 1);
+  function clickTab(type) {
+    setCurrentTab(type);
   }
 
   return (
@@ -46,17 +29,12 @@ function App() {
         </aside>
         <section>
           <nav>
-            <Tab name="Packs" item={gameState.packstab}></Tab>
-            <Tab name="Trade binder" item={gameState.tradebindertab}></Tab>
+            <Tab name="Packs" onClick={() => clickTab(<PacksTab />)} item={gameState.packstab}></Tab>
+            <Tab name="Trade binder" onClick={() => clickTab(<TradebinderTab />)} item={gameState.tradebindertab}></Tab>
             <Tab name="Tournaments" item={gameState.tournamentstab}></Tab>
             <Tab name="Skills" item={gameState.skillstab}></Tab>
           </nav>
-          <article>
-          <Button text="Open pack" type="buy" click={openPack} resource={gameState.money} cost={packCostRule.value}></Button>
-          <Button text="Sell bad card" type="sell" click={sellBadCards} resource={gameState.badcards} cost={badSellValueRule.value}></Button>
-          <Button text="Sell good card" type="sell" click={sellGoodCards} resource={gameState.goodcards} cost={goodSellValueRule.value}></Button>
-          <Button text="Sell meta card" type="sell" click={sellMetaCards} resource={gameState.metacards} cost={metaSellValueRule.value}></Button>
-          </article>
+          {currentTab}
         </section>
       </div>
       <footer>
