34e0291a74aea6544fdfa504d16a36c70ae9fc52
Gusten Andersson <gusten.andersson@momentum.se>
2019-07-04 13:06:42 +0200

    first commit of buying / selling bulk buttons

--- src/components/button.js
+++ src/components/button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import BulkButton from './bulkbutton';
 
 export default function Button(props) {
     function clickEvent(e) {
@@ -7,13 +8,20 @@ export default function Button(props) {
             props.click();
     }
 
-    if(!props.resource.acquired)
+    if (!props.resource.acquired)
         return null;
 
+    let x10 = <div />;
+    if (props.resource.amount / props.cost > 10)
+        x10 = <BulkButton amount={10} click={clickEvent}></BulkButton>
+
     return (
-        <div className="button" onClick={clickEvent}>
-            { props.text }
-            <div className="button-cost"> { props.cost } money</div>
+        <div className="button-container">
+            <div className="button" onClick={clickEvent}>
+                {props.text}
+                <div className="button-cost"> {props.cost} money</div>
+            </div>
+            {x10}
         </div>
     )
 }
