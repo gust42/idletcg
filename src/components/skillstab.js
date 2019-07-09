5f7518d8add666322dcdafaf991ba9615da725a2
Gusten Andersson <gusten.andersson@momentum.se>
2019-07-09 11:49:19 +0200

    Better skilltab

--- src/components/skillstab.js
+++ src/components/skillstab.js
@@ -12,13 +12,21 @@ export default function SkillsTab(props) {
         MessageHandler.recieveMessage('unlockskill', 'autopackskill');
     }
 
+    let unlocked = '';
+    let unlockDiv = (<div><div className="requirement">Requires {gameRule.requirement} badcards</div>
+        <div className="unlock">Unlock</div></div>);
+    if (gameState.autopackskill.acquired) {
+        unlocked = 'unlocked';
+        unlockDiv = null;
+    }
+
     return (
         <article>
-            <div onClick={unlockSkill} className="skill button">
+            <div onClick={unlockSkill} className={'skill button ' + unlocked}>
                 <div className="title">Wealthy boyfriend / girlfriend</div>
-                <div className="description">Doesnt like to play but loves to buy and open packs for you</div>
-                <div className="requirement">Requires 1500 badcards</div>
-                <div className="unlock">Unlock</div>
+                <div className="description">Doesnt like to play but loves to gift and open packs for you</div>
+
+                {unlockDiv}
             </div>
         </article>
     )
