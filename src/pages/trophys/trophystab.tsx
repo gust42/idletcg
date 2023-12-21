560ef71933d461b18ea345df1926b71cc8128102
Thunman <Thunman42@gmail.com>
2023-12-21 18:39:30 +0100

    adding slots to trophys

--- src/pages/trophys/trophystab.tsx
+++ src/pages/trophys/trophystab.tsx
@@ -1,6 +1,29 @@
+import { Slot } from "../deckbuilder/slot"
+import useGameState from "../../hooks/usegamestate";
+import MessageHandler from "../../logic/messagehandler";
+import { state } from "../../state/state";
+
+
 export default function TrophysTab(){
-    return(
-    <div>
-        Trophys
-    </div>)
-}
+    const gameState = useGameState();
+    const t: number = Object.keys(state.trophys).length;
+    const onSelect = (id: number | undefined, slot: number) => {
+        //placeholder to get it to run
+    };
+    return (
+        <div className="flex flex-row flex-wrap gap-2">
+          {Array.from({ length: t }).map((_, i) => {
+            const index = `slot${i + 1}` as keyof typeof gameState.trophys;
+            return (
+              <Slot
+                onSelect={onSelect}
+                key={index}
+                card={gameState.trophys[index]}
+                slot={i + 1}
+              />
+            );
+          })}
+        </div>
+      );
+    
+};
